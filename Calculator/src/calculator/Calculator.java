4d751338ff550fe92b4fd0c46dacc5d4f0535007
Kun <jufangting@gmail.com>
2019-04-03 09:57:55 +0200

    This is the source code.

---
+++ Calculator/src/calculator/Calculator.java
@@ -0,0 +1,29 @@
+package calculator;
+
+import java.util.Scanner;
+
+import calculator.operations.Operation;
+
+public class Calculator {
+
+	public static void main(String[] args) {
+
+		Scanner s = new Scanner(System.in);
+		System.out.print("Insert first number");
+		double a = s.nextDouble(); s.nextLine();
+		System.out.print("Insert the operator: ");
+		String op = s.nextLine();
+		System.out.print("Insert second number: ");
+		double b = s.nextDouble();
+		
+		Operation o = Operation.parseOperator(op);
+		if(o == null) {
+			System.err.println("Operator non recognized!");
+		} else {
+			o.setNumbers(a, b);
+			System.out.println("Result of " + o.getName() + " is " + o.calc());
+		}
+		s.close();
+	}
+
+}
